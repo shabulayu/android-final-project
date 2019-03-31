0be501d0f1471070b6d693cb35170042fe3a27a9
siyuan yu <ysyfisher@gmail.com>
2019-03-31 11:34:42 -0400

    NewsFeed v1.0(internet data) by Siyuan Yu

---
+++ app/src/main/java/com/example/finalproject/NewsFeed/NewsArticle.java
@@ -0,0 +1,15 @@
+package com.example.finalproject.NewsFeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.finalproject.R;
+
+public class NewsArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_article);
+    }
+}
